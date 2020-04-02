96e6a1fa187297df53331d2793e949e4dea10e02
Joss31AF <fuentes.joce15@gmail.com>
2020-04-02 13:56:19 -0600

    avance

---
+++ src/app/models/revista.ts
@@ -0,0 +1,8 @@
+export class Revista {
+
+    numRevista: string;
+    urlPDF: string;
+    urlPortada: string;
+  
+
+}
--- src/app/unpload-file/unpload-file.component.ts
+++ src/app/unpload-file/unpload-file.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Numero17Component } from '../numero17/numero17.component';
+
+import { HostListener } from '@angular/core';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-unpload-file',
   templateUrl: './unpload-file.component.html',
   styleUrls: ['./unpload-file.component.css']
 })
+
 export class UnploadFileComponent implements OnInit {
 
   constructor() { }
 
+  
+  
+
   ngOnInit() {
   }
 
+
+ 
+
+  validacion(){
+
+    let numRev = (document.getElementById('NumRev') as HTMLInputElement).value;
+    let fileUnpPDF = (document.getElementById('file-unpload-pdf') as HTMLInputElement).value;
+    let fileUnpImg = (document.getElementById('file-unpload-img') as HTMLInputElement).value;
+
+    if(numRev==''){
+       window.alert("Numero De Revista Vacio!!!");
+    }else if(fileUnpPDF==''){
+      window.alert("No se Eligio Un Archivo PDF");
+    }else if(fileUnpImg==''){
+      window.alert("No se Eligio Una Imagen De Portada");
+    }else{
+
+
+
+
+
+
+
+
+
+
+    }
+    
+  }
+
+  valLetNum(){
+
+    return true;
+
+   
+   
+  }
 }
