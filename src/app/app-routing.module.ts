2b202198c682de5572b678ef526644a146f2aa57
BrandonTRN <brtn918@gmail.com>
2020-04-24 19:25:11 -0500

    Cambio dentro de post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,18 +12,6 @@ import {EditorialComponent} from './editorial/editorial.component'
 import{InicioComponent} from './inicio/inicio.component'
 import{LineamientosComponent} from './lineamientos/lineamientos.component'
 import {NAnterioresComponent} from './n-anteriores/n-anteriores.component'
-import { Numero30Component } from './numero30/numero30.component';
-import { Numero34Component } from './numero34/numero34.component';
-import { Numero46Component } from './numero46/numero46.component';
-import { Numero43Component } from './numero43/numero43.component';
-import { Numero42Component } from './numero42/numero42.component';
-import { Numero44Component } from './numero44/numero44.component';
-import { Numero17Component } from './numero17/numero17.component';
-import { Numero19Component } from './numero19/numero19.component';
-import { Numero20Component } from './numero20/numero20.component';
-import { Numero35Component } from './numero35/numero35.component';
-import { Numero40Component } from './numero40/numero40.component';
-import { Numero41Component } from './numero41/numero41.component';
 import { UploadFileComponent } from './upload-file/upload-file.component';
 import { ActualizaRevistaComponent } from './actualiza-revista/actualiza-revista.component';
 
@@ -58,56 +46,7 @@ const routes: Routes = [
 path:  'Anteriores',
 component:  NAnterioresComponent
 },
-
-    {
-      path:  'Numero46',
-      component:  Numero46Component
-      },
-    {
-      path:  'Numero30',
-      component:  Numero30Component
-      },
-      {
-        path:  'Numero40',
-        component:  Numero40Component
-        },
-      {
-        path:  'Numero35',
-        component:  Numero35Component
-        },
-      {
-        path:  'Numero34',
-        component:  Numero34Component
-        },
-        {
-          path:  'Numero44',
-          component:  Numero44Component
-          },
-        {
-          path:  'Numero43',
-          component:  Numero43Component
-          },
-          {
-            path:  'Numero42',
-            component:  Numero42Component
-            },
-          {
-            path:  'Numero41',
-            component:  Numero41Component
-            },
-            {
-              path:  'Numero20',
-              component:  Numero20Component
-              },
-            {
-              path:  'Numero19',
-              component:  Numero19Component
-              },
-            {
-              path:  'Numero17',
-              component:  Numero17Component
-              },
-              
+            
               {
                 path:  'Administrador',
                 component:  AdministradorComponent
